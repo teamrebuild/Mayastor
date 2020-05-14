77be1bd54c98bc62f74172427f4309bf4633b7c6
paulyoong <paul.yoong@mayadata.io>
2020-05-14 14:13:47 +0100

    Tidy up unit tests

--- mayastor/tests/io_arbiter.rs
+++ mayastor/tests/io_arbiter.rs
@@ -3,8 +3,7 @@ extern crate log;
 
 pub mod common;
 
-use crossbeam::channel::{after, select, unbounded};
-use futures::channel::oneshot;
+use crossbeam::channel::{after, select, unbounded, Receiver, Sender};
 use mayastor::{
     bdev::{nexus_create, nexus_lookup},
     core::{
@@ -34,6 +33,32 @@ fn test_ini() {
         create_nexus().await;
     });
 }
+
+macro_rules! init_multiple_reactors {
+    () => {
+        common::MSTEST.get_or_init(|| {
+            common::mayastor_test_init();
+            MayastorEnvironment::new(MayastorCliArgs {
+                reactor_mask: "0x3".to_string(),
+                ..Default::default()
+            })
+            .init()
+        });
+    };
+}
+
+fn test_init_multiple_reactors() {
+    init_multiple_reactors!();
+    for i in 0 .. NUM_NEXUS_CHILDREN {
+        common::delete_file(&[get_disk(i)]);
+        common::truncate_file_bytes(&get_disk(i), NEXUS_SIZE);
+    }
+
+    Reactor::block_on(async {
+        create_nexus().await;
+    });
+}
+
 fn test_fini() {
     for i in 0 .. NUM_NEXUS_CHILDREN {
         common::delete_file(&[get_disk(i)]);
@@ -65,13 +90,32 @@ async fn create_nexus() {
     }
 }
 
-// Sleep for the specified duration then signal the reactor that the sleep has
-// completed.
-fn sleep_and_alert(d: Duration, r: &Reactor, s: oneshot::Sender<()>) {
-    sleep(d);
-    r.send_future(async move {
-        let _ = s.send(());
-    })
+// Sets each of the reactors to the running state so they are available for use.
+// This function doesn't run the reactors, that has to be done by calling "poll"
+// outside of this.
+fn init_reactors() -> Vec<&'static Reactor> {
+    let mut reactors = Vec::new();
+    for r in Reactors::iter() {
+        reactors.push(r);
+        r.running();
+    }
+
+    reactors
+}
+
+// Function should be called after an expected timeout in the test.
+// This sends a signal to clean up and waits for the test to complete.
+// The clean up logic needs to be implemented in the individual tests.
+fn cleanup_after_timeout(s: Sender<()>, r: Receiver<()>) {
+    trace!("Send cleanup message");
+    let _ = s.send(());
+    reactor_poll!(100);
+
+    // Expect cleanup to succeed
+    select! {
+        recv(r) -> _ => println!("Cleanup succeeded"),
+        recv(after(Duration::from_secs(1))) -> _ => panic!("Cleanup timed
+    out"), }
 }
 
 #[test]
@@ -79,61 +123,90 @@ fn sleep_and_alert(d: Duration, r: &Reactor, s: oneshot::Sender<()>) {
 fn lock_unlock() {
     let (s, r) = unbounded::<()>();
 
-    std::thread::spawn(move || {
+    let _ = std::thread::spawn(move || {
         test_ini();
-        Reactor::block_on(async move {
+
+        let reactor = Reactors::current();
+        reactor.running();
+        reactor.send_future(async move {
             let mut arbiter = IoArbiter::new(NEXUS_NAME);
             let mut ctx = arbiter.lock(1, 5).await;
             arbiter.unlock(&mut ctx).await;
             let _ = s.send(());
         });
-        test_fini();
-    });
+        reactor_poll!(100);
 
-    select! {
-        recv(r) -> _ => trace!("Success"),
-        recv(after(Duration::from_secs(1))) -> _ => panic!("Timed out"),
-    }
+        select! {
+            recv(r) -> _ => trace!("Success"),
+            recv(after(Duration::from_secs(1))) -> _ => panic!("Timed out"),
+        }
+
+        test_fini();
+    })
+    .join();
 }
 
 #[test]
-// Test multiple lock requests for an overlapping LBA.
-// Unlocking is not performed, therefore only the first lock request should
+// Test making multiple request for a lock for an overlapping LBA range.
+// Unlocking is not performed therefore only the first lock request should
 // succeed.
 fn multiple_locks() {
     let (s, r) = unbounded::<()>();
+    let (cleanup_s, cleanup_r) = unbounded::<()>();
 
-    std::thread::spawn(move || {
-        test_ini();
-        Reactor::block_on(async move {
+    let _ = std::thread::spawn(move || {
+        test_init_multiple_reactors();
+
+        let fut1 = async move {
             let mut arbiter = IoArbiter::new(NEXUS_NAME);
-            arbiter.lock(1, 10).await;
-            arbiter.lock(1, 5).await;
+            let mut ctx = arbiter.lock(1, 10).await;
 
+            // Cleanup after test finished
+            let _ = cleanup_r.recv();
+            trace!("Received cleanup message");
+            arbiter.unlock(&mut ctx).await;
+        };
+
+        let fut2 = async move {
+            let mut arbiter = IoArbiter::new(NEXUS_NAME);
+            let mut ctx = arbiter.lock(1, 5).await;
+
+            arbiter.unlock(&mut ctx).await;
             let _ = s.send(());
-        });
-        test_fini();
-    });
+        };
 
-    // Expect a timeout - the first lock request is never unlocked, so the
-    // second lock request should never succeed.
-    select! {
-        recv(r) -> _ => panic!("Shouldn't succeed on double lock"),
-        recv(after(Duration::from_secs(1))) -> _ => println!("Success"),
-    }
+        let reactors = init_reactors();
+        assert_eq!(reactors.len(), 2);
+
+        reactors[0].send_future(fut1);
+        reactors[0].poll_once();
+
+        // Reactor 1 is polling by default
+        reactors[1].send_future(fut2);
+
+        // Expect a timeout - the first lock request is never unlocked, so the
+        // second lock request should never succeed.
+        select! {
+            recv(r) -> _ => panic!("Shouldn't succeed on double lock"),
+            recv(after(Duration::from_secs(1))) -> _ => println!("Success"),
+        }
+
+        cleanup_after_timeout(cleanup_s, r);
+        test_fini();
+    })
+    .join();
 }
 
 #[test]
 // Test multiple lock requests for an overlapping LBA range with unlocking.
-// The first lock is acquired and only unlocked after the timer times out.
+// The first lock is acquired and is only unlocked after the timer times out.
 // Whilst the first lock is held, the second lock request should be blocked.
 // The second lock request should only succeed after the first lock is unlocked.
 fn multiple_locks_with_unlocks() {
     let (s, r) = unbounded::<()>();
-    let (timer_sender, timer_receiver) = oneshot::channel::<()>();
 
-    std::thread::spawn(move || {
-        test_ini();
+    let _ = std::thread::spawn(move || {
+        test_init_multiple_reactors();
 
         // Acquire lock and hold it for an extended duration.
         let fut1 = async move {
@@ -143,14 +216,14 @@ fn multiple_locks_with_unlocks() {
             let mut ctx = arbiter.lock(1, 10).await;
             trace!("fut1: ACQUIRED LOCK");
 
-            // Wait for timer to expire
-            let _ = timer_receiver.await;
-
+            // Wait for timer to expire before unlocking
+            //let _ = timer_receiver.recv();
+            sleep(Duration::from_secs(1));
             arbiter.unlock(&mut ctx).await;
             trace!("fut1: RELEASED LOCK");
         };
 
-        // Attempt to acquire the lock whilst it is already being held.
+        // Attempt to acquire the lock
         let fut2 = async move {
             let mut arbiter = IoArbiter::new(NEXUS_NAME);
 
@@ -165,49 +238,52 @@ fn multiple_locks_with_unlocks() {
             let _ = s.send(());
         };
 
-        // Configure the reactor
-        Reactors::init();
-        let reactor = Reactors::current();
-        reactor.running();
-        reactor.send_future(fut1);
-        reactor.send_future(fut2);
+        let reactors = init_reactors();
+        assert_eq!(reactors.len(), 2);
 
-        // Set up timer thread before polling the reactor and running the above
-        // futures.
-        std::thread::spawn(move || {
-            sleep_and_alert(Duration::from_secs(1), &reactor, timer_sender);
-        });
+        reactors[0].send_future(fut1);
+        reactors[0].poll_once();
 
-        reactor.poll_reactor();
+        reactors[1].send_future(fut2);
+
+        reactor_poll!(1000);
+
+        // The second lock request should succeed and unlock once the first lock
+        // is unlocked.
+        select! {
+            recv(r) -> _ => trace!("Success"),
+            recv(after(Duration::from_secs(5))) -> _ => panic!("Timed out"),
+        }
 
         test_fini();
-    });
-
-    select! {
-        recv(r) -> _ => trace!("Success"),
-        recv(after(Duration::from_secs(5))) -> _ => panic!("Timed out"),
-    }
+    })
+    .join();
 }
 
 #[test]
 // Test issuing a front-end I/O to a LBA range that overlaps a locked range.
-// The front-end I/O should never complete because the lock is never unlocked.
+// The front-end I/O should never complete because the lock is neverunlocked.
 fn fe_io_lock_held() {
     let (s, r) = unbounded::<()>();
-    let (s1, r1) = oneshot::channel::<()>();
+    let (s1, r1) = unbounded::<()>();
+    let (cleanup_s, cleanup_r) = unbounded::<()>();
 
-    std::thread::spawn(move || {
-        test_ini();
+    let _ = std::thread::spawn(move || {
+        test_init_multiple_reactors();
 
-        // Future 1, acquire lock
+        // Acquire the lock
         let fut1 = async move {
             let mut arbiter = IoArbiter::new(NEXUS_NAME);
-            arbiter.lock(1, 5).await;
+            let mut ctx = arbiter.lock(1, 5).await;
             trace!("fut1: LOCKED");
             let _ = s1.send(());
+
+            // Cleanup
+            let _ = cleanup_r.recv();
+            arbiter.unlock(&mut ctx).await;
         };
 
-        // Future 2, issue I/O to nexus
+        // Issue front-end write I/O
         let fut2 = async move {
             let nexus_desc = Bdev::open_by_name(&NEXUS_NAME, true).unwrap();
             let h = nexus_desc.into_handle().unwrap();
@@ -216,8 +292,10 @@ fn fe_io_lock_held() {
             let blk_size = 512;
             let buf = DmaBuf::new(blk * blk_size, 9).unwrap();
 
+            trace!("fut2: running");
+
             // Wait for fut1 to obtain lock
-            let _ = r1.await;
+            let _ = r1.recv();
 
             trace!("fut2: write to nexus");
             match h.write_at((blk * blk_size) as u64, &buf).await {
@@ -228,23 +306,25 @@ fn fe_io_lock_held() {
             let _ = s.send(());
         };
 
-        // Configure the reactor
-        Reactors::init();
-        let reactor = Reactors::current();
-        reactor.running();
-        reactor.send_future(fut1);
-        reactor.send_future(fut2);
-        reactor.poll_reactor();
+        let reactors = init_reactors();
+        assert_eq!(reactors.len(), 2);
 
+        reactors[0].send_future(fut1);
+        reactors[0].poll_once();
+
+        reactors[1].send_future(fut2);
+
+        // Expect a timeout - the nexus shouldn't be able to be written to while
+        // a lock is held on an overlapping LBA range
+        select! {
+            recv(r) -> _ => panic!("Shouldn't be able to write to nexus"),
+            recv(after(Duration::from_secs(1))) -> _ => println!("Success"),
+        };
+
+        cleanup_after_timeout(cleanup_s, r);
         test_fini();
-    });
-
-    // Expect a timeout - the nexus shouldn't be able to be written to while a
-    // lock is held on an overlapping LBA range
-    select! {
-        recv(r) -> _ => panic!("Shouldn't be able to write to nexus"),
-        recv(after(Duration::from_secs(5))) -> _ => println!("Success"),
-    }
+    })
+    .join();
 }
 
 #[test]
@@ -253,27 +333,26 @@ fn fe_io_lock_held() {
 // The front-end I/O should eventually complete after the lock is unlocked.
 fn fe_io_lock_unlock() {
     let (s, r) = unbounded::<()>();
-    let (s1, r1) = oneshot::channel::<()>();
-    let (timer_sender, timer_receiver) = oneshot::channel::<()>();
+    let (s1, r1) = unbounded::<()>();
 
-    std::thread::spawn(move || {
-        test_ini();
+    let _ = std::thread::spawn(move || {
+        test_init_multiple_reactors();
 
-        // Future 1, acquire lock and unlock after delay
+        // Acquire the lock and unlock after delay
         let fut1 = async move {
             let mut arbiter = IoArbiter::new(NEXUS_NAME);
             let mut ctx = arbiter.lock(1, 5).await;
             info!("fut1: LOCKED");
             let _ = s1.send(());
 
-            // Wait for timer to expire
-            let _ = timer_receiver.await;
-
+            // Wait for timer to expire before unlocking
+            //let _ = timer_receiver.recv();
+            sleep(Duration::from_secs(1));
             arbiter.unlock(&mut ctx).await;
             info!("fut1: UNLOCKED");
         };
 
-        // Future 2, issue I/O to nexus
+        // Issue a write I/O to the nexus
         let fut2 = async move {
             let nexus_desc = Bdev::open_by_name(&NEXUS_NAME, true).unwrap();
             let h = nexus_desc.into_handle().unwrap();
@@ -283,7 +362,7 @@ fn fe_io_lock_unlock() {
             let buf = DmaBuf::new(blk * blk_size, 9).unwrap();
 
             // Wait for f1 to obtain lock
-            let _ = r1.await;
+            let _ = r1.recv();
 
             info!("fut2: write to nexus");
             match h.write_at((blk * blk_size) as u64, &buf).await {
@@ -294,26 +373,92 @@ fn fe_io_lock_unlock() {
             let _ = s.send(());
         };
 
-        // Configure the reactor
-        Reactors::init();
-        let reactor = Reactors::current();
-        reactor.running();
-        reactor.send_future(fut1);
-        reactor.send_future(fut2);
+        let reactors = init_reactors();
+        assert_eq!(reactors.len(), 2);
 
-        // Set up timer thread before polling the reactor and running the above
-        // futures.
-        std::thread::spawn(move || {
-            sleep_and_alert(Duration::from_secs(1), &reactor, timer_sender);
-        });
+        reactors[0].send_future(fut1);
+        reactors[0].poll_once();
 
-        reactor.poll_reactor();
+        reactors[1].send_future(fut2);
+
+        reactor_poll!(100);
+
+        // The write to the nexus should complete once the first lock is
+        // unlocked
+        select! {
+            recv(r) -> _ => println!("Success"),
+            recv(after(Duration::from_secs(5))) -> _ => panic!("Expected front end write to succeed"),
+        }
 
         test_fini();
-    });
+    }).join();
+}
 
-    select! {
-        recv(r) -> _ => println!("Success"),
-        recv(after(Duration::from_secs(5))) -> _ => panic!("Expected front end write to succeed"),
-    }
+#[test]
+// Test issuing a front-end I/O which acquires a lock over the LBA range and
+// then attempt to request a lock for an overlapping range. The front-end I/O
+// then releases the lock.
+fn fe_io_holding_lock_unlock() {
+    let (s, r) = unbounded::<()>();
+
+    let _ = std::thread::spawn(move || {
+        test_init_multiple_reactors();
+
+        // Future 1, issue write I/O to nexus
+        let fut1 = async move {
+            let nexus_desc = Bdev::open_by_name(&NEXUS_NAME, true).unwrap();
+            let h = nexus_desc.into_handle().unwrap();
+
+            let blk = 2;
+            let blk_size = 512;
+            let buf = DmaBuf::new(blk * blk_size, 9).unwrap();
+
+            trace!("fut1: write to nexus");
+            match h.write_at((blk * blk_size) as u64, &buf).await {
+                Ok(_) => trace!("Successfully wrote to nexus"),
+                Err(e) => trace!("Failed to write to nexus: {}", e),
+            }
+        };
+
+        // Future 2, request lock
+        let fut2 = async move {
+            let mut arbiter = IoArbiter::new(NEXUS_NAME);
+            trace!("fut2: REQUEST LOCK");
+            arbiter.lock(1, 5).await;
+            trace!("fut2: LOCKED");
+            let _ = s.send(());
+        };
+
+        // Configure the reactor
+        Reactors::init();
+        let  reactors = init_reactors();
+        assert_eq!(reactors.len(), 2);
+
+        // Run the front-end I/O.
+        // Only poll once to allow the I/O to be issued but not to complete.
+        // This ensures the lock is held for an extended period.
+        reactors[0].send_future(fut1);
+        reactors[0].poll_once();
+
+        // Attempt to acquire the lock
+        reactors[1].send_future(fut2);
+
+        // Lock request should time out - the front-end I/O is still holding the lock
+        select! {
+            recv(r) -> _ => panic!("Shouldn't be able to acquire lock"),
+            recv(after(Duration::from_secs(5))) -> _ => println!("Success"),
+        }
+
+        // Now the lock request has timed out, poll to allow the front-end I/O
+        // to complete and release the lock
+        reactor_poll!(100);
+
+        // Lock request should succeed - the front-end I/O has completed
+        select! {
+            recv(r) -> _ => println!("Success"),
+            recv(after(Duration::from_secs(5))) -> _ => panic!("Shouldn't be able to acquire lock"),
+        }
+
+        test_fini();
+    }).join();
 }
